c2bd2921db1cec431312c4dbcedb7a0c80143c28
Matheus Rodrigues <mmrodrigues077@gmail.com>
2023-11-18 21:12:40 -0300

    Trabalhando na base do projeto

---
+++ ApiPersonagensRM/Server/Controllers/CharactersController.cs
@@ -0,0 +1,41 @@
+﻿using ApiPersonagensRM.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiPersonagensRM.Server.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+        private HttpClient _httpClient;
+        public CharactersController() 
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<Characters> GetAllCharacters()
+        {
+            try
+            {
+                Characters characters = null;
+                HttpResponseMessage httpResponseMessage = await 
+                    _httpClient.GetAsync("https://rickandmortyapi.com/api/character");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                characters = JsonConvert.DeserializeObject<Characters>
+                    (responseBody);
+
+                return characters;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ApiPersonagensRM/Shared/Models/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApiPersonagensRM.Shared.Models
+{
+    public class Characters
+    {
+        public Info info { get; set; }
+        public List<Character> results {  get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string? next { get; set; }
+        public string? prev { get; set; }
+    }
+}
